263435975bd5c3cb4105e494cacad1ce26954272
sanjajovic <162323859+sanjajovic@users.noreply.github.com>
2025-10-06 15:05:58 +0200

    height fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@
 import Layout from "./components/layout";
 
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
+  }, []);
 
   return (
     <div className="body">
