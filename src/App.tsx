cfb87f585db5ea35bc35eee19604fbff95f9b5af
sanjajovic <162323859+sanjajovic@users.noreply.github.com>
2025-10-06 15:14:11 +0200

    padding fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,8 @@
 import Layout from "./components/layout";
 
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    setVh();
-    window.addEventListener("resize", setVh);
-    return () => window.removeEventListener("resize", setVh);
-  }, []);
-
   return (
     <div className="body">
       <Layout />
